d6a995faca1d641cfa681b900527f511e0575e26
Igy Lizardo <igylizardo@gmail.com>
2023-02-15 23:07:13 -0800

    working towards adding range slider

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # etch-a-sketch
 etch a sketch project Odin Project
+
+for part 3 function call with arguments height width, height is i, width is j -> make square maker into function
--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+
+
 const container = document.querySelector(".container");
 const row = [];
 const col = [];
+
+//nested for loop for creating grid of div boxes
 for (let i = 0; i < 16; i++) {
     row[i] = document.createElement('div');
     row[i].classList.add('row');
@@ -13,7 +17,7 @@ for (let i = 0; i < 16; i++) {
     }
 }
 
-
+//turn div boxes black on mouseover
 const box = document.querySelectorAll(".box");
 box.forEach((item, index) => {
     item.addEventListener('mouseover', () => {
@@ -21,9 +25,15 @@ box.forEach((item, index) => {
             });
 })
 
+//button click to set all boxes to white
 const btn = document.querySelector("#reset");
 btn.addEventListener('click', () => {
     box.forEach((item) => {
         item.style.cssText = "background-color:white;";
     })
+})
+
+const range = document.querySelector("#sizeSlider");
+range.addEventListener('input', () => {
+    range.innerHTML = range.value;
 })
