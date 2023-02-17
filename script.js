144b0498a2db7601db6cc6f521cce9e1fc1b9e69
Igy Lizardo <igylizardo@gmail.com>
2023-02-16 22:39:46 -0800

    etch creates off of slider but needs to reset

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
-
-
 const container = document.querySelector(".container");
+const btn = document.querySelector("#reset");
+const range = document.querySelector("#sizeSlider");
+const label = document.querySelector("#rangeValue");
+
+
 const row = [];
 const col = [];
 
+
+
 //nested for loop for creating grid of div boxes
-for (let i = 0; i < 16; i++) {
-    row[i] = document.createElement('div');
-    row[i].classList.add('row');
-    container.appendChild(row[i]);
-    for (let j = 0; j < 16; j++) {
-        col[j] = document.createElement('div');
-        col[j].classList.add('box');
-        row[i].appendChild(col[j]);
-        
+//need to make into a function that is called so after range is changed
+//need to reset etch everytime updated
+function createEtch(size = 16) {
+    for (let i = 0; i < size; i++) {
+        row[i] = document.createElement('div');
+        row[i].classList.add('row');
+        container.appendChild(row[i]);
+        for (let j = 0; j < size; j++) {
+            col[j] = document.createElement('div');
+            col[j].classList.add('box');
+            row[i].appendChild(col[j]);
+            
+        }
     }
 }
 
-//turn div boxes black on mouseover
+
 const box = document.querySelectorAll(".box");
+//turn div boxes black on mouseover
 box.forEach((item, index) => {
     item.addEventListener('mouseover', () => {
                 item.style.cssText= "background-color:black;";
@@ -26,14 +36,13 @@ box.forEach((item, index) => {
 })
 
 //button click to set all boxes to white
-const btn = document.querySelector("#reset");
 btn.addEventListener('click', () => {
     box.forEach((item) => {
         item.style.cssText = "background-color:white;";
     })
 })
 
-const range = document.querySelector("#sizeSlider");
+
 range.addEventListener('input', () => {
-    range.innerHTML = range.value;
+    label.innerHTML = range.value + " x " + range.value;
 })
