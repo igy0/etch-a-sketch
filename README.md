be16019729fcada6f89d15fcafc82d1a957258ff
Igy Lizardo <igylizardo@gmail.com>
2023-02-18 16:33:24 -0800

    added delete board to reset

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 etch a sketch project Odin Project
 
 for part 3 function call with arguments height width, height is i, width is j -> make square maker into function
+square maker needs to reset everytime
+create counter for everytime slider is changed for if else statement wtihin createboard, use to initialize board and delete old one if board is already made
--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const container = document.querySelector(".container");
-const btn = document.querySelector("#reset");
 const range = document.querySelector("#sizeSlider");
 const label = document.querySelector("#rangeValue");
-
+const btn = document.querySelector("#reset");
 
 const row = [];
 const col = [];
@@ -12,7 +11,8 @@ const col = [];
 //nested for loop for creating grid of div boxes
 //need to make into a function that is called so after range is changed
 //need to reset etch everytime updated
-function createEtch(size = 16) {
+function createBoard(size) {
+
     for (let i = 0; i < size; i++) {
         row[i] = document.createElement('div');
         row[i].classList.add('row');
@@ -24,24 +24,44 @@ function createEtch(size = 16) {
             
         }
     }
+    const box = document.querySelectorAll(".box");
+    box.forEach((item, index) => {
+        item.addEventListener('mouseover', () => {
+                    item.style.cssText= "background-color:black;";
+                });
+    })
+    
+}
+function deleteBoard() {
+    const boxes = document.querySelectorAll(".box")
+    boxes.forEach((item) => {
+        item.remove();
+    })
 }
 
-
-const box = document.querySelectorAll(".box");
+// const box = document.querySelectorAll(".box");
 //turn div boxes black on mouseover
-box.forEach((item, index) => {
-    item.addEventListener('mouseover', () => {
-                item.style.cssText= "background-color:black;";
-            });
-})
-
+// box.forEach((item, index) => {
+//     item.addEventListener('mouseover', () => {
+//                 item.style.cssText= "background-color:black;";
+//             });
+// })
 //button click to set all boxes to white
 btn.addEventListener('click', () => {
+    resetBoard();
+    deleteBoard();
+});
+// btn.addEventListener('click', () => {
+//     box.forEach((item) => {
+//         item.style.cssText = "background-color:white;";
+//     })
+// })
+function resetBoard() {
+    const box = document.querySelectorAll(".box");
     box.forEach((item) => {
         item.style.cssText = "background-color:white;";
     })
-})
-
+}
 
 range.addEventListener('input', () => {
     label.innerHTML = range.value + " x " + range.value;
